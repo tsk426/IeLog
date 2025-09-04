cbb2f70d31dc34b7091bbffdb169d8e9527a17ae
EC2 Default User <ec2-user@ip-172-31-8-170.ap-northeast-1.compute.internal>
2025-09-04 14:40:40 +0000

    fix logout flow

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,24 @@
 class Public::HomesController < ApplicationController
-  def top
+
+  before_action :redirect_to_confirm_if_signed_in, only: :top
+
+  def top; end
+  def about; end
+
+  def confirm_logout_to_home
+    redirect_to homes_top_path unless user_signed_in? || admin_signed_in?
   end
 
-  def about
+  def logout_to_home
+    sign_out_all_scopes
+    reset_session
+    redirect_to homes_top_path, notice: 'ログアウトしました（トップページに戻りました）'
   end
+
+  private
+
+  def redirect_to_confirm_if_signed_in
+    redirect_to confirm_logout_to_home_path if user_signed_in? || admin_signed_in?
+  end
+
 end
